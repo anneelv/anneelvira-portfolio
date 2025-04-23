f3f2b311afe1bc2c736d61370863410268bce0e1
anneelv <anneelvira.lauw@gmail.com>
2025-04-23 21:41:18 +1000

    Fix issue with invalid GET request

--- personal-portfolio/src/app/api/portfolio/[id]/route.ts
+++ personal-portfolio/src/app/api/portfolio/[id]/route.ts
@@ -4,9 +4,9 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   request: NextRequest,
-  context: { params: { id: string } } //Here we are passing the number of the portfolio as a string parameter for the request
+  { params }: { params: { id: string } } //Here we are passing the number of the portfolio as a string parameter for the request
 ) {
-  const { id } = context.params;
+  const { id } = params;
   // In case of using a real dataase, instead of .find (JavaScript Higher Order function) we will use 'SELECT ... FROM ...'
   const portfolioItem = portfolioData.find(
     // matching the numerical data of id acquired from Portfolio Data to String data that we want to get from the request ^
