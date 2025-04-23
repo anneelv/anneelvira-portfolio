62673ba56af8a1dc57cced209a3a0ca1e47f6b41
anneelv <anneelvira.lauw@gmail.com>
2025-04-23 21:24:32 +1000

    Fix issue with invalid GET export in portfolio-route.ts

--- personal-portfolio/src/app/api/portfolio/[id]/route.ts
+++ personal-portfolio/src/app/api/portfolio/[id]/route.ts
@@ -2,13 +2,14 @@
 import { portfolioData } from "@/app/data/data";
 
 export async function GET(
-  _request: Request,
-  { params }: { params: { id: string } } //Here we are passing the number of the portfolio as a string parameter for the request
+  request: Request,
+  context: { params: { id: string } } //Here we are passing the number of the portfolio as a string parameter for the request
 ) {
+  const { id } = context.params;
   // In case of using a real dataase, instead of .find (JavaScript Higher Order function) we will use 'SELECT ... FROM ...'
   const portfolioItem = portfolioData.find(
     // matching the numerical data of id acquired from Portfolio Data to String data that we want to get from the request ^
-    (item: { id: number }) => item.id.toString() === params.id
+    (item: { id: number }) => item.id.toString() === id
   );
 
   return Response.json(portfolioItem);
